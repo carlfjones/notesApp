ebc9b511b45264c3deb52a39adf30b992e6df612
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-07-09 17:20:55 +0100

    display empty NoteList

--- spec/noteListViewSpec.js
+++ spec/noteListViewSpec.js
@@ -2,6 +2,14 @@ function testingNoteListView() {
   var noteList = new NoteList();
   var noteListView =  new NoteListView(noteList);
   assert.isTrue(noteListView.list === noteList);
-}
+};
+
+function testingNoteListViewDisplay() {
+  var noteList = new NoteList();
+  var noteListView =  new NoteListView(noteList);
+  console.log(noteListView.display());
+  assert.isTrue(noteListView.display() === `<ul></ul>`)
+};
 
 testingNoteListView();
+testingNoteListViewDisplay();
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -3,5 +3,13 @@
     this.list = list;
   };
 
+  NoteListView.prototype.display = function () {
+    if (this.list.notes.length === 0) {
+      var htmlString = `<ul></ul>`
+    }
+    // var htmlString = `<ul><li><div>${this.list.notes.join("")}</div></li></ul>`
+    return htmlString;
+  };
+
   exports.NoteListView = NoteListView
 })(this);
