a1066fef12c8f232400406de8f85f4c92cc88368
Arthur Carl Forest-Jones <carlfj@live.co.uk>
2019-07-10 16:10:29 +0100

    Updated note-controller with runApp, need debugging

---
+++ note-controller.js
@@ -0,0 +1,10 @@
+function runApp() {
+  var app = document.getElementById('app');
+  console.log("WATTUPPPPPP");
+  console.log(app);
+  app.innerHTML = "Howdy";
+};
+
+
+console.log("hey");
+runApp();
