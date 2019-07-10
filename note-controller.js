ee66567b8a6edf80e2eb1bc3c812b72a54cc916a
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-07-10 17:56:22 +0100

    start debugging process for NoteController

--- note-controller.js
+++ note-controller.js
@@ -1,6 +1,24 @@
-function runApp() {
-  var app = document.getElementById("app");
-  app.innerHTML = "Howdy";
-};
+(function(exports) {
+  function NoteController() {
+    var noteList = new NoteList();
+    var noteListView = new NoteListView(noteList)
+    noteList.createAndStore("Favourite drink: Irn-Bru");
+  };
 
-runApp();
+  NoteController.prototype.displayOnPage = function () {
+    var note = noteListView.display();
+    var app = document.getElementById("app");
+    app.innerHTML = note;
+  };
+
+  exports.NoteController = NoteController
+})(this);
+
+var list = new NoteList();
+var listView = new NoteController(list);
+console.log("hello3");
+console.log(list);
+console.log("hello4");
+console.log(listView);
+console.log("hello5");
+listView.displayOnPage();
