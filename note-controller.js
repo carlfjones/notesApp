acdc3b40050011c5583b235a63619717fac43360
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-07-11 17:01:05 +0100

    TDD displaying note

--- note-controller.js
+++ note-controller.js
@@ -1,6 +1,14 @@
 (function(exports) {
-  function NoteController() {
-    
+  function NoteController(aNoteList = NoteList, aNoteListView = NoteListView) {
+    this.noteList = new aNoteList
+    this.noteList.createAndStore("I like ham")
+    this.noteListView = new aNoteListView(this.NoteList)
   };
+
+  NoteController.prototype.displayOnPage = function() {
+    var element = document.getElementById("app");
+    element.innerHTML = this.noteListView.display();
+  };
+
   exports.NoteController = NoteController;
 })(this);
--- spec/noteControllerSpec.js
+++ spec/noteControllerSpec.js
@@ -7,6 +7,31 @@ console.log("Note Controller");
     assert.isTrue(noteController instanceof NoteController);
   };
 
+  function testingNoteControllerChangesInnerHTML() {
+    function MockNoteList() {
+      this.createAndStore = function() {};
+    };
+
+    function MockNoteListView() {
+      this.display = function() {
+        return "<ul><li><div>I like ham</div></li></ul>"
+      };
+    };
+
+    var noteController = new NoteController(MockNoteList, MockNoteListView);
+
+
+    noteController.displayOnPage();
+    var element = document.getElementById("app");
+    console.log(element);
+    console.log(element.innerHTML);
+    assert.isTrue(element.innerHTML === "<ul><li><div>I like ham</div></li></ul>")
+  };
+
+
+
   testingNoteControllerIsAnInstanceOfNoteController();
+  testingNoteControllerChangesInnerHTML();
+
 
 })(this);
